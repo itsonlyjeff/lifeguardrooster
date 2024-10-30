418da6ec27d0550aca931bf570965ecabcd3abca
Jeffrey van Reenen <jeffrey92.hrb@gmail.com>
2024-10-30 13:48:43 +0100

    Base Installation

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Jeffrey van Reenen',
+            'email' => 'jeffrey92.hrb@gmail.com',
+            'password' => Hash::make('Kikie1'),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
